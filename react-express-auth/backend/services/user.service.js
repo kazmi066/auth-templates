c324347159cd324860819acc0edc3a584129e846
kazmi066 <awaisabbas47@gmail.com>
2022-02-01 11:05:49 +0500

    password hashing

--- react-express-auth/backend/services/user.service.js
+++ react-express-auth/backend/services/user.service.js
@@ -1,11 +1,13 @@
-const bcryptjs = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const ApiError = require("../utils/ApiError");
 
 const createUser = async (reqBody) => {
     const isEmailExists = await User.isEmailExists(reqBody.email);
     if (isEmailExists) throw new ApiError(401, "Email already exists");
-    return User.create(reqBody);
+    const { password } = reqBody;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    return User.create({ ...reqBody, password: hashedPassword });
 }
 
 const getUserByEmail = async (email) => {
@@ -13,7 +15,7 @@ const getUserByEmail = async (email) => {
 }
 
 const comparePassword = (password, userPassword) => {
-    const passwordCheck = bcryptjs.compareSync(password, userPassword);
+    const passwordCheck = bcrypt.compareSync(password, userPassword);
     if (!passwordCheck) throw new ApiError(401, "Invalid password");
     return true;
 }
--- react-express-auth/frontend/src/axiosClient.js
+++ react-express-auth/frontend/src/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'http://localhost:4000/api/v1/',
     withCredentials: true
 })
 
