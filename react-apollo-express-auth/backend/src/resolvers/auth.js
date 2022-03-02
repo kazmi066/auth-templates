fbc3ea6926fd7753107deaed0353dad6d3ee18cf
kazmi066 <awaisabbas47@gmail.com>
2022-03-02 08:19:30 +0500

    update: cookie send

--- react-apollo-express-auth/backend/src/resolvers/auth.js
+++ react-apollo-express-auth/backend/src/resolvers/auth.js
@@ -61,6 +61,10 @@ export default {
 
         const token = createToken(user, process.env.JWT_SECRET, '30h');
 
+        res.cookie('token', token, {
+          maxAge: 1000 * 60 * 60 * 24 * 30,     // 30 days
+        })
+        
         return { token };
 
       } catch (error) {
