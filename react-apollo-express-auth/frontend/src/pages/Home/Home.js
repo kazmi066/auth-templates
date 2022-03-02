aa041995651fa9c122ae59e218adc191314da302
kazmi066 <awaisabbas47@gmail.com>
2022-03-02 11:32:36 +0500

    final changes

--- react-apollo-express-auth/frontend/src/pages/Home/Home.js
+++ react-apollo-express-auth/frontend/src/pages/Home/Home.js
@@ -1,11 +1,7 @@
-import useAuth from "../../context/AuthContext"
-
 export default function Home() {
-    const { logout, user } = useAuth();
     return (
         <section id="home">
             Home component
-            {user && <button onClick={logout}>Logout</button>}
         </section>
     )
 }
--- react-apollo-express-auth/frontend/src/routes.js
+++ react-apollo-express-auth/frontend/src/routes.js
@@ -2,8 +2,6 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
-import Dashboard from './pages/Dashboard/Dashboard';
-import PrivateRoute from "./PrivateRoute";
 
 export default function MyRoutes() {
     return (
@@ -12,9 +10,6 @@ export default function MyRoutes() {
             <Route path="auth">
                 <Route path="login" element={<Login />} />
                 <Route path="register" element={<Register />} />
-            </Route>
-            <Route path="/dashboard" element={<PrivateRoute />}>
-                <Route path="/dashboard" element={<Dashboard />} />
             </Route>
         </Routes >
     )
